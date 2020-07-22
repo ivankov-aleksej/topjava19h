b57c099d861909269a7e51fd3472d78128fe42e0
ivankov_aleksey <ivankov.aleksej@gmail.com>
2020-07-22 15:51:28 +0300

    for heroku

---
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.service.datajpa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import static ru.javawebinar.topjava.Profiles.DATAJPA;
+import static ru.javawebinar.topjava.UserTestData.*;
+
+@ActiveProfiles(DATAJPA)
+class DataJpaUserServiceTest extends AbstractUserServiceTest {
+    @Test
+    void getWithMeals() throws Exception {
+        User admin = service.getWithMeals(ADMIN_ID);
+        USER_MATCHER.assertMatch(admin, ADMIN);
+        MealTestData.MEAL_MATCHER.assertMatch(admin.getMeals(), MealTestData.ADMIN_MEAL2, MealTestData.ADMIN_MEAL1);
+    }
+
+    @Test
+    void getWithMealsNotFound() throws Exception {
+        Assertions.assertThrows(NotFoundException.class,
+                () -> service.getWithMeals(1));
+    }
+}
